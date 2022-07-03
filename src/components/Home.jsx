4578d97dfc551cf4292dbbdec71ff19fc6c611b2
Boorclark <clarkg@berea.edu>
2022-07-03 02:47:23 -0400

    fixed route for button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-orange-600 hover:border-orange-600'>
-            <Link to='skills' smooth={true} duration={500}>
+            <Link to='work' smooth={true} duration={500}>
               View Work
             </Link>
             <span className='group-hover:rotate-90 duration-300'>
