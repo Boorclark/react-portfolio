bc5dc447b2e17fdafc6410221d65f8d07ce823e2
Boorclark <clarkg@berea.edu>
2022-07-03 12:48:04 -0400

    Added in "this website" in projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import WorkImg from '../assets/workImg.PNG';
-
+import reactPortfolio from '../assets/reactPortfolio.PNG'
 
 const Work = () => {
   return (
@@ -33,6 +33,24 @@ const Work = () => {
                   </button>
                 </a>
                 <a href='https://github.com/Boorclark/Blog'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${reactPortfolio})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                This Website!
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://github.com/Boorclark/react-portfolio'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
