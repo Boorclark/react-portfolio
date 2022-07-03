3110621ebf3faa6aa80a54e42f84f587b39b83f2
Boorclark <clarkg@berea.edu>
2022-07-03 18:16:36 -0400

    added hover effect to skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,6 +14,7 @@ import Python from '../assets/python.png'
 import SQLite from '../assets/sqlite.png'
 import Git from '../assets/git.png'
 
+
 const Skills = () => {
   return (
     <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
@@ -23,52 +24,52 @@ const Skills = () => {
               <p className='text-4xl font-bold inline border-b-4 border-orange-600 '>Technical Skills</p>
               <p className='py-4'>// These are the technologies I've worked with</p>
           </div>
-          <div className='w-full flex items-end grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-              <div>
+          <div className='w-full items-end grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+              <div className='hover:scale-110 duration-500'>
                 <img className='w-20 mx-auto' src={Python} alt="HTML icon" />
                 <p className='my-4'>PYTHON</p>
               </div>
-              <div>
+              <div className='hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={JavaScript} alt="JAVASCRIPT icon" />
                   <p className='my-4'>JAVASCRIPT</p>
               </div>
-              <div>
+              <div className='hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                   <p className='my-4'>HTML</p>
               </div>
-              <div>
+              <div className='hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
                   <p className='my-4'>CSS</p>
               </div>
-              <div>
+              <div className='hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={GitHub} alt="GITHUB icon" />
                   <p className='my-4'>GITHUB</p>
               </div>
-              <div>
+              <div className='hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Git} alt="GIT icon" />
                   <p className='my-4'>GIT</p>
               </div>
-              <div>
+              <div className='hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Flask} alt="FLASK icon" />
                   <p className='my-4'>FLASK</p>
               </div>
-              <div>
+              <div className='hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={ReactImg} alt="REACT icon" />
                   <p className='my-4'>REACT</p>
               </div>
-              <div>
+              <div className='hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
                   <p className='my-4'>TAILWIND CSS</p>
               </div>
-              <div>
+              <div className='hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Bootstrap} alt="BOOTSRAP icon" />
                   <p className='my-4'>BOOTSTRAP</p>
               </div>
-              <div>
+              <div className='hover:scale-110 duration-500'>
                   <img className='w-40 mx-auto' src={SQLite} alt="SQLITE icon" />
                   <p className='my-4'>SQLITE</p>
               </div>
-              <div>
+              <div className='hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={MySQL} alt="MySQL icon" />
                   <p className='my-4'>MYSQL</p>
               </div>
