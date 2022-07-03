fb599d5fa6a3e4587159ebd7c2838b0f1cd1b4a2
Boorclark <clarkg@berea.edu>
2022-07-03 17:47:52 -0400

    Added in slide ins on hope page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,15 +2,36 @@ import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import { Link } from 'react-scroll';
 import Typewriter from 'typewriter-effect';
+import MovingComponent from 'react-moving-text'
 
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-[#ccd6f6] font-bold'>Hello! My name is</p>
+        <p className='text-[#ccd6f6] font-bold'>
+          <MovingComponent
+            type="slideInFromRight"
+            duration="1100ms"
+            delay="index * 100ms"
+            direction="normal"
+            timing="ease"
+            iteration="1"
+            fillMode="none">
+            Hello! My name is
+          </MovingComponent>
+        </p>
         <h1 className='text-4xl sm:text-7xl font-bold text-orange-600'>
-           Garrett Clark.
+          <MovingComponent
+            type="slideInFromLeft"
+            duration="1100ms"
+            delay="index * 100ms"
+            direction="normal"
+            timing="ease"
+            iteration="1"
+            fillMode="none">
+            Garrett Clark
+          </MovingComponent>
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
           <Typewriter
