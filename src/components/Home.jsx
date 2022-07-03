eeccf6573b0f6883f1f491b5634603d82f826c52
Boorclark <clarkg@berea.edu>
2022-07-02 22:13:27 -0400

    added and changed about me and home discription

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-pink-600'>Hi, my name is</p>
+        <p className='text-pink-600'>Hello! My name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
            Garrett Clark
         </h1>
@@ -14,9 +14,9 @@ const Home = () => {
           I'm a Software Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-        I'm a rising sophomore computer science major at Berea College who is passionate about how technology can 
-        improve the quality of life of those who use it. I hope to continue to delve into this passion throughout 
-        my education and career through internships and employment opportunities. 
+        I'm a software developer who builds and designs exceptional user-focused applications and digital experiences. 
+        Currently, I am achieving this through developing an application for Berea College's CELTS program alongside a 
+        team of excellent software developers.
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
