8adf7962c9253d550333bc329c3a50309cd08d38
Boorclark <clarkg@berea.edu>
2022-07-03 18:04:26 -0400

    added home animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,24 +34,51 @@ const Home = () => {
           </MovingComponent>
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-          <Typewriter
-            options={{
-              strings: ["I'm a Software Developer.", "I'm a CS Student.", "I'm an Active Learner."],
-              autoStart: true,
-              loop: true,
-            }}
-          />
+        <MovingComponent
+            type="jelly"
+            duration="1100ms"
+            delay="index * 100ms"
+            direction="normal"
+            timing="ease"
+            iteration="1"
+            fillMode="none">
+            <Typewriter
+              options={{
+                strings: ["I'm a Software Developer.", "I'm a CS Student.", "I'm an Active Learner."],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </MovingComponent>
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I'm a software developer who builds and designs exceptional user-focused applications and digital experiences. 
-          Currently, I am achieving this through developing an application for Berea College's CELTS program alongside a 
-          team of excellent software developers.
+          <MovingComponent
+            type="jelly"
+            duration="1100ms"
+            delay="index * 100ms"
+            direction="normal"
+            timing="ease"
+            iteration="1"
+            fillMode="none">
+            I'm a software developer who builds and designs exceptional user-focused applications and digital experiences. 
+            Currently, I am achieving this through developing an application for Berea College's CELTS program alongside a 
+            team of excellent software developers.
+          </MovingComponent>
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-orange-600 hover:border-orange-600'>
-            <Link to='work' smooth={true} duration={500}>
-              View Work
-            </Link>
+            <MovingComponent
+              type="jelly"
+              duration="1100ms"
+              delay="index * 100ms"
+              direction="normal"
+              timing="ease"
+              iteration="1"
+              fillMode="none">
+              <Link to='work' smooth={true} duration={500}>
+                View Work
+              </Link>
+            </MovingComponent>
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
             </span>
