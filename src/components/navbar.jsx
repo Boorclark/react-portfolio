1f2f5204f896400f572bbd8243c3e9d2c7736fb3
Boorclark <clarkg@berea.edu>
2022-07-03 19:47:42 -0400

    reactive navbar hover color change

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -17,27 +17,27 @@ const Navbar = () => {
       {/* menu */}
       <ul className='hidden md:flex'>
         <li>
-          <Link to='home' smooth={true} duration={500}>
+          <Link to='home' smooth={true} duration={500} className="hover:text-orange-600">
             Home
           </Link>
         </li>
         <li>
-          <Link to='about' smooth={true} duration={500}>
+          <Link to='about' smooth={true} duration={500} className="hover:text-orange-600">
             About
           </Link>
         </li>
         <li>
-          <Link to='skills' smooth={true} duration={500}>
+          <Link to='skills' smooth={true} duration={500} className="hover:text-orange-600">
             Skills
           </Link>
         </li>
         <li>
-          <Link to='work' smooth={true} duration={500}>
+          <Link to='work' smooth={true} duration={500} className="hover:text-orange-600">
             Projects
           </Link>
         </li>
         <li>
-          <Link to='contact' smooth={true} duration={500}>
+          <Link to='contact' smooth={true} duration={500} className="hover:text-orange-600">
             Contact
           </Link>
         </li>
