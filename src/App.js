54b56fe496f9a5497a55d1d6bb682b2074159c68
Boorclark <clarkg@berea.edu>
2022-07-04 00:11:04 -0400

    created custom cursor

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
     <div >
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={16}
+        color="12, 158, 234"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={4}       
+      />
       <Navbar />
       <Home />
       <About />
