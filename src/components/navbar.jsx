cf0bf610b4807cdfeed7524b2e93902af153107f
Boorclark <clarkg@berea.edu>
2022-07-10 16:13:01 -0400

    changed mobile navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -59,7 +59,13 @@ const Navbar = () => {
         }
       >
         <li className='py-6 text-4xl'>
-        <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
